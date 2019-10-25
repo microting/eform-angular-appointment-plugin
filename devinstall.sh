3d593035eef8ed4e645f6893d91ca410e594ac30
MathiasHusum <mathiashusumn@gmail.com>
2019-10-25 08:19:22 +0200

    Create devinstall.sh

---
+++ devinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~
+pwd
+
+if [ -d "Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/appointment-pn" ]; then
+	rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/appointment-pn
+fi
+
+cp -av Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/src/app/plugins/modules/appointment-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/appointment-pn
+
+if [ -d "Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Appointment.Pn" ]; then
+	rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Appointment.Pn
+fi
+
+cp -av Documents/workspace/microting/eform-angular-appointment-plugin/eFormAPI/Plugins/Appointment.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Appointment.Pn
