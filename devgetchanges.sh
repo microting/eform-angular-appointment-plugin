38b0d934a78e4d9abc9bbbec05e9675be45ba271
René Schultz Madsen <rm@microting.dk>
2020-04-01 18:10:59 +0200

    Adding changes to make it pass on travis for chromedriver and angular 8.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -2,14 +2,23 @@
 
 cd ~
 
-if [ -d "Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/src/app/plugins/modules/appointment-pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/src/app/plugins/modules/appointment-pn
-fi
+rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/src/app/plugins/modules/appointment-pn
 
-cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/appointment-pn Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/src/app/plugins/modules/appointment-pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/appointment-pn Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/src/app/plugins/modules/appointment-pn
 
-if [ -d "Documents/workspace/microting/eform-angular-appointment-plugin/eFormAPI/Plugins/Appointment.Pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eFormAPI/Plugins/Appointment.Pn
-fi
+rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eFormAPI/Plugins/Appointment.Pn
 
-cp -av Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Appointment.Pn Documents/workspace/microting/eform-angular-appointment-plugin/eFormAPI/Plugins/Appointment.Pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Appointment.Pn Documents/workspace/microting/eform-angular-appointment-plugin/eFormAPI/Plugins/Appointment.Pn
+
+# Test files rm
+rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/e2e/Tests/appointment-settings/
+rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/e2e/Tests/appointment-general/
+rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/e2e/Page\ objects/Monitoring/
+rm -fR Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+
+# Test files cp
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/appointment-settings Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/e2e/Tests/appointment-settings
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/appointment-general Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/e2e/Tests/appointment-general
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/Appointment Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/e2e/Page\ objects/Appointment
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-appointment-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+
